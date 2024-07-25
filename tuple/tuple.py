6d61513de7ef184ddabd6786ebdffcec49d6417c
lilmoficodes <lilmoficodes@gmail.com>
2024-07-25 11:01:54 +0530

    Tuple

---
+++ tuple/tuple.py
@@ -0,0 +1,13 @@
+a= ();
+b =(1,); #empty tuple
+c = ("Mofi", "Lamborghini", 56, 434.9, True)
+# print(type(a));
+# print(type(b));
+# print(c,"\n", type(c));
+print(c.count(56)) # it returns number of count
+print(c.index(56)) # looks for the index for the value
+# d= (5,4,3, 6)
+# e = (45,34,56,34,23,45,76)
+# concate = d+e;
+# print(concate)
+
